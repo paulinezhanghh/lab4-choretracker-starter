4329afb79982f25d0375b1507274377f562c5cbb
Your Name <your@email.address>
2024-02-06 18:37:44 -0500

    changed

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class ChildTest < ActiveSupport::TestCase
+  
+  #relationships
+  should have_many(:chores)
+  should have_many(:tasks).through(:chores)
+
   # test "the truth" do
   #   assert true
   # end

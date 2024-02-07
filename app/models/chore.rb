7ed59a8473219e2647257afee1814d5db1a21362
Your Name <your@email.address>
2024-02-06 20:23:14 -0500

    updaate

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,7 +2,20 @@ class Chore < ApplicationRecord
     belongs_to :task
     belongs_to :child
 
+    validates_date :due_on
 
-    scope : by_task, {join(:task).order('task.name')}
+    scope :by_task, -> {joins(:task).order('name')}
+    scope :chronological, -> { order(:due_on) }
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where('due_on >= ?', Date.today) }
+    scope :past, -> { where('due_on < ?', Date.today) }
 
+    def status
+        if completed == false
+            return "Pending"
+        else
+            return "Completed"
+        end
+    end
 end

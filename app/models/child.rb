4da203971f3aecb77a90f5b341073fcaf77a4520
Your Name <your@email.address>
2024-02-06 19:19:20 -0500

    child model updated

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,7 +5,10 @@ class Child < ApplicationRecord
     validates_presence_of :first_name
     validates_presence_of :last_name
 
+    scope :alphabetical, -> { order('last_name, first_name') }
+    scope :active, -> { where(active:true) }
+
     def name
-        return "First Last"
+        return first_name+" "+last_name
       end
 end
